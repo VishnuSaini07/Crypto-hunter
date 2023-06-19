525a274fe8d3005f450b0c1ef6d0fbed8ee11c58
Vishnu Saini <61437139+VishnuSaini07@users.noreply.github.com>
2023-06-19 20:09:11 +0530

    Second commit

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: `${process.env.REACT_APP_API_KEY}`,
-    authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
-    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
-    storageBucket: `${process.env.REACT_STORAGE_BUCKET}`,
-    messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
-    appId:`${process.env.REACT_APP_APP_ID}`
+    apiKey: "AIzaSyD18UDJdBItiZnN5jSOpIBz5U6ZHl4FK7U",
+    authDomain: "crypto-hunter-5e4a1.firebaseapp.com",
+    projectId: "crypto-hunter-5e4a1",
+    storageBucket: "crypto-hunter-5e4a1.appspot.com",
+    messagingSenderId: "173421784649",
+    appId:"1:173421784649:web:60682505875797b4669998"
 };
 
 export default firebaseConfig;
